7bab1a1dc0ae101123463aa2f33107b3c750e66d
cwade <christopher.d.wade@wustl.edu>
2014-12-23 19:24:33 -0600

    Basic start and class file read

--- src/main/java/com/wade/decompile/Decompiler.java
+++ src/main/java/com/wade/decompile/Decompiler.java
@@ -1,6 +1,28 @@
 package com.wade.decompile;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Decompiler {
+    public Decompiler(String[] args) throws IOException {
+        DataInputStream in = new DataInputStream(new FileInputStream(new File(args[0])));
+        ClassFileData data = new ClassFileData(in);
+        in.close();
+    }
+
+    private void disassemble() {
+    }
+
     public void main(String... args){
+        try {
+            Decompiler dc = new Decompiler(args);
+            dc.disassemble();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
