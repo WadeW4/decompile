ac72be8184e321c66ffec0087e552a40afc58710
Christopher D. Wade <christopher.d.wade@wustl.edu>
2015-01-05 13:25:26 -0600

    adding decompiler stuff

--- decompile-sample-app/src/main/java/com/wade/decompile/Decompiler.java
+++ decompile-sample-app/src/main/java/com/wade/decompile/Decompiler.java
@@ -1,30 +1,42 @@
 package com.wade.decompile;
 
-import org.apache.bcel.classfile.ClassParser;
-import org.apache.bcel.classfile.JavaClass;
-
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Utility;
+
 public class Decompiler {
+    private JavaClass javaClass;
+
     public Decompiler(String[] args) throws IOException {
-        DataInputStream in = new DataInputStream(new FileInputStream(new File(args[0])));
-        ClassParser parser = new ClassParser(args[0]);
-        JavaClass javaClass = parser.parse();
-        System.out.println(javaClass.toString());
+	ClassParser parser = new ClassParser(args[0]);
+	javaClass = parser.parse();
     }
 
-    private void disassemble() {
+    private void disassemble() throws ClassNotFoundException {
+	StringBuffer buf = new StringBuffer();
+
+	String access = Utility.accessToString(javaClass.getAccessFlags(), true);
+	access = access.equals("") ? "" : (access + " ");
+
+	String classOrInterface = Utility.classOrInterface(javaClass.getAccessFlags());
+	String superClass = Utility.compactClassName(javaClass.getSuperclassName(), false);
+	String className = javaClass.getClassName();
+	String interfaceClasses = Utility.getListOfInterfaces(javaClass.getAllInterfaces());
+
+	buf.append(String.format("%s%s %s extends %s implements %s {\n", access, classOrInterface, className, superClass, interfaceClasses));
+	buf.append("}\n");
+
+	System.out.println(buf.toString());
     }
 
     public static void main(String... args) {
-        try {
-            Decompiler dc = new Decompiler(args);
-            dc.disassemble();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	try {
+	    Decompiler dc = new Decompiler(args);
+	    dc.disassemble();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
     }
 }
